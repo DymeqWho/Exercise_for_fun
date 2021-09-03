aad9b16090e5203b65cc38fd1cdde3c57d21a821
Dymitr_Misiejuk <dymitrmisiejuk@gmail.com>
2021-09-03 21:11:42 +0200

    Collections Overview with Comparator and Comparable

--- src/collections/lists/theatre/Main.java
+++ src/collections/lists/theatre/Main.java
@@ -1,21 +1,86 @@
 package collections.lists.theatre;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
-//        theatre.getSeats();
+        checkTheSeatAndPayIfOK(theatre, "D12");
+        checkTheSeatAndPayIfOK(theatre, "D12");
+        checkTheSeatAndPayIfOK(theatre, "Z12");
 
-        checkTheSeatAndPayIfOK(theatre, "H11");
-        checkTheSeatAndPayIfOK(theatre, "H11");
+        List<Theatre.Seat> reverseSeat = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeat);
+        printList(reverseSeat);
+
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
+
+//        checkTheSeatAndPayIfOK(theatre, "H11");
+//        checkTheSeatAndPayIfOK(theatre, "H11");
+
+//        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+//        printList(seatCopy);
+//
+//        seatCopy.get(1).reserve();
+//        checkTheSeatAndPayIfOK(theatre, "A02");
+//
+//        Collections.reverse(seatCopy);
+//        System.out.println("Printing seatCopy");
+//        printList(seatCopy);
+//        System.out.println("Printing theatre.seat");
+//        printList(theatre.seats);
+//
+//        Collections.shuffle(seatCopy);
+//        System.out.println("Printing seatCopy shuffle");
+//        printList(seatCopy);
+//
+//        Theatre.Seat minSeat = Collections.min(seatCopy);
+//        Theatre.Seat maxSeat = Collections.max(seatCopy);
+//        System.out.println(minSeat.getSeatNumber() + " min seat");
+//        System.out.println(maxSeat.getSeatNumber()  + " max seat");
+//
+//        sortList(seatCopy);
+//        System.out.println("Pritning sorted seatCopy");
+//        printList(seatCopy);
     }
 
     public static void checkTheSeatAndPayIfOK(Theatre theatre, String seatNumber) {
+        char[] chars = seatNumber.toCharArray();
+        int numberOver13 = Integer.parseInt(chars[1] + "" + chars[2]);
         if (theatre.reserveSeat(seatNumber)) {
-            System.out.println("Please pay");
+            System.out.println("Please pay for " + seatNumber);
+        } else if (numberOver13 >= 13 || chars[0] > 'H') {
+            System.out.print("");
         } else {
-            System.out.println("Sorry, seat is taken");
+            System.out.println("Sorry, seat " + seatNumber + " is already taken");
         }
     }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("===============");
+    }
+
+//    public static void sortList(List<? extends Theatre.Seat> list){
+//        for (int i=0; i<list.size() -1; i++){
+//            for (int j = i+1; j< list.size(); j++){
+//                if(list.get(i).compareTo(list.get(j))>0){
+//                    Collections.swap(list, i, j);
+//                }
+//            }
+//        }
+//    }
 }
--- src/collections/lists/theatre/Theatre.java
+++ src/collections/lists/theatre/Theatre.java
@@ -7,12 +7,26 @@ public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
 
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            return (int) (seat1.getPrice() - seat2.getPrice());
+        }
+    };
+
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
         int lastRow = 'A' + (numRows - 1);
         for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                double price = 12.00;
+
+                if ((row > 'D') && (seatNum >= 4 && seatNum <= 9)) {
+                    price = 14.00;
+                } else if ((row > 'F') || (seatNum < 4 || seatNum > 9)) {
+                    price = 7.00;
+                }
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                 seats.add(seat);
             }
         }
@@ -23,12 +37,12 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = new Seat(seatNumber);
+        Seat requestedSeat = new Seat(seatNumber, 0);
         int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
 
-        if(foundSeat >= 0){
+        if (foundSeat >= 0) {
             return seats.get(foundSeat).reserve();
-        }else {
+        } else {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
@@ -47,18 +61,24 @@ public class Theatre {
     }
 
     // for testing
-    public void getSeats() {
-        for (Seat seat : seats) {
-            System.out.println(seat.getSeatNumber());
-        }
+    public
+    //void
+    Collection<Seat> getSeats() {
+//        for(Seat seat: seats){
+//            System.out.println(seat.getSeatNumber());
+//        }
+        return seats;
     }
 
-    private static class Seat implements Comparable<Seat>{
+
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
+        private double price;
         private boolean reserved = false;
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
         }
 
         @Override
@@ -88,6 +108,10 @@ public class Theatre {
 
         public String getSeatNumber() {
             return seatNumber;
+        }
+
+        public double getPrice() {
+            return price;
         }
     }
 }
